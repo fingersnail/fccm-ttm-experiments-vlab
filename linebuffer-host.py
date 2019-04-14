6828ed52c8e5739469121a84ef6c91ee765a8780
Zhang Weihao <weihao@iam-ssh1.research.intel-research.net>
2019-04-14 10:15:11 -0700

    fix bugs

--- linebuffer-host.py
+++ linebuffer-host.py
@@ -5,14 +5,14 @@ import sys
 emul = 0
 
 if emul:
-  I=16, J=16 ,II=8, JJ=8, KX=3, KY=3, SI=1, SJ=1
+  I,J,II,JJ,KX,KY,SI,SJ=16,16,8,8,3,3,1,1
 
 else:
-  I=16, J=16 ,II=8, JJ=8, KX=3, KY=3, SI=1, SJ=1
+  I,J,II,JJ,KX,KY,SI,SJ=16,16,8,8,3,3,1,1
 
 AOCX_FILE = "linebuffer.aocx"
 
-command = 'g++ -g -O0 -D__USE_XOPEN2K8 -Wall -IALSDK/include -I/usr/local/include -DHAVE_CONFIG_H -DTESTB -g -LALSDK/lib  -L/usr/local/lib  -fPIC -I../common/inc -I../extlibs/inc -I/export/quartus_pro/17.1.1/hld/host/include  linebuffer-host.cpp -L/export/quartus_pro/17.1.1/hld/linux64/lib -L/export/fpga/release/a10_gx_pac_ias_1_1_pv/opencl/opencl_bsp/linux64/lib  -L/export/quartus_pro/17.1.1/hld/host/linux64/lib -Wl,--no-as-needed -lalteracl  -lintel_opae_mmd -lrt -lelf -L/opt/aalsdk/sdk502/lib -Wl,-rpath=/opt/aalsdk/sdk502/lib -DI={} -DJ={} -DKX={} -DKY={} -DII={} -DJJ={} -DSI={} -DSJ={} -DAOCX_FILE=\\"{}\\"'.format(I,J,KX,KY,II,JJ,SI,SJ,AOCX_FILE) + " -o linebuffer 2>&1 | tee -a host.log"
+command = 'g++ -g -O0 -D__USE_XOPEN2K8 -Wall -IALSDK/include -I/usr/local/include -DHAVE_CONFIG_H -DTESTB -g -LALSDK/lib  -L/usr/local/lib  -fPIC -I../common/inc -I../extlibs/inc -I/export/quartus_pro/17.1.1/hld/host/include  linebuffer-host.cpp -L/export/quartus_pro/17.1.1/hld/linux64/lib -L/export/fpga/release/a10_gx_pac_ias_1_1_pv/opencl/opencl_bsp/linux64/lib  -L/export/quartus_pro/17.1.1/hld/host/linux64/lib -Wl,--no-as-needed -lalteracl  -lintel_opae_mmd -lrt -lelf -L/opt/aalsdk/sdk502/lib -Wl,-rpath=/opt/aalsdk/sdk502/lib -DI={} -DJ={} -DKX={} -DKY={} -DII={} -DJJ={} -DSI={} -DSJ={} -DAOCX_FILE=\\"{}\\"'.format(I,J,KX,KY,II,JJ,SI,SJ,AOCX_FILE) + " -o host-emul 2>&1 | tee -a host.log"
 
 print command
 
--- run-drain.py
+++ run-drain.py
@@ -11,14 +11,13 @@ else:
 # Note: II has to be 1 else the compute buffer cannot be implemented as rotating register
 
 if (emul):
-  I=16, J=16 ,II=8, JJ=8, KX=3, KY=3, SI=1, SJ=1
+  I,J,II,JJ,KX,KY,SI,SJ=16,16,8,8,3,3,1,1
 
 else:
-  I=16, J=16 ,II=8, JJ=8, KX=3, KY=3, SI=1, SJ=1
+  I,J,II,JJ,KX,KY,SI,SJ=16,16,8,8,3,3,1,1
 
 aocx_name = "linebuffer"
 
-$(FILE).cl -o $(FILE).aocx
 command = "qsub-aoc {} {}.cl -o {}.aocx 2>&1 | tee -a run.log".format(aoc_opt, aocx_name, aocx_name)
 
 os.system("echo "+command)
