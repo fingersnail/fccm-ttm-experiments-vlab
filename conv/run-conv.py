41d7a737da01374b440c658f24bbce6569799109
Zhang Weihao <weihao@iam-ssh1.research.intel-research.net>
2019-05-30 23:25:01 -0700

    change scale

--- conv/run-conv.py
+++ conv/run-conv.py
@@ -11,13 +11,13 @@ else:
 # Note: II has to be 1 else the compute buffer cannot be implemented as rotating register
 
 if (emul):
-  BATCH,NOX,NOY,NIF,NOF,POX,POY,POF,KX,KY,SX,SY=16,128,128,16,64,8,8,8,3,3,1,1
+  BATCH,NOX,NOY,NIF,NOF,POX,POY,POF,KX,KY,SX,SY=16,128,128,4,64,8,8,8,3,3,1,1
 else:
-  BATCH,NOX,NOY,NIF,NOF,POX,POY,POF,KX,KY,SX,SY=16,128,128,16,64,8,8,8,3,3,1,1
+  BATCH,NOX,NOY,NIF,NOF,POX,POY,POF,KX,KY,SX,SY=16,128,128,4,64,8,8,8,3,3,1,1
 
 aocx_name = "conv3-64"
 
-command = "qsub-aoc {} -DBATCH={} -DNOX={} -DNOY={} -DNIF={} -DNOF={} -DPOX={} -DPOY={} -DPOF={} -DKX={} -DKY={} -DSX={} -DSY={} {}.cl -o {}.aocx 2>&1 | tee -a run.log".format(aoc_opt,BATCH,NOX,NOY,NIF,NOF,POX,POY,POF,KX,KY,SX,SY,aocx_name,aocx_name)
+command = "qsub-aoc {} -DFLOAT_VEC=float4 -DBATCH={} -DNOX={} -DNOY={} -DNIF={} -DNOF={} -DPOX={} -DPOY={} -DPOF={} -DKX={} -DKY={} -DSX={} -DSY={} {}.cl -o {}.aocx 2>&1 | tee -a run.log".format(aoc_opt,BATCH,NOX,NOY,NIF,NOF,POX,POY,POF,KX,KY,SX,SY,aocx_name,aocx_name)
 
 os.system("echo "+command)
 os.system(command)
